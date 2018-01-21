21a44802c9536b69679b7352e32c752ed2bb7237
InALunch <email.linch@gmail.com>
2018-01-21 02:31:11 -0800

    added the 'open_triplets' strategy

--- solver.py
+++ solver.py
@@ -70,7 +70,7 @@ def canceller(matrix, cancellation_queue):
         updates = update_matrix(matrix, pos)
         for update in updates:
             cancellation_queue.append(update)
-    return matrix, cancellation_queue
+    return matrix
     
 def count_line(matrix, line):
     update = set()
@@ -141,6 +141,7 @@ def extracttuple(n):
     
 def find_twins(line, matrix):
     '''
+    Given a list of positions in the same 'block' (line/square) where the matrix stores sets of length 2, identify "open twins", ie points where the stored set at those positions are identical.
      Basically just a variant algorithm of the optimal solution to TwoSum, see https://leetcode.com/problems/two-sum/description/ 
     '''
     unmatched = dict()
@@ -152,6 +153,26 @@ def find_twins(line, matrix):
         else:
             unmatched[value] = elem
     return twins 
+
+def find_triplets(tripleslist, matrix):
+    '''Identifies and returns the first set of triplets in a block, akin to "find_twins".
+    We stop searching after the first set of triplets is found, since it seems exceedingly unlikely that the same block will have two triplets'''
+    unmatched = dict()
+    triplets = []
+    for elem in tripleslist:
+        value = tuple(matrix[elem[0]][elem[1]])
+        if value in unmatched:
+            if len(unmatched[value]) == 2:
+                triplets.append(unmatched[value][0])
+                triplets.append(unmatched[value][1])
+                triplets.append(elem)
+                return triplets
+            else:
+                unmatched[value].append(elem)
+        else:
+            unmatched[value] = [elem]
+    return []
+    
 
 def tuple_ellimination(block, matrix, tups):
     values = matrix[tups[0][0]][tups[0][1]]
@@ -166,18 +187,42 @@ def tuple_ellimination(block, matrix, tups):
     return updates 
         
 def open_twins(blocks, matrix):
+    '''
+    1. For each block, find 'open twins', that is, positions in the same block where the underlying possibilities are identical sets of length two.
+    eg, (0,5) and (0,7) are in the same row. Suppose that matrix[0][5] and matrix[0][7] both point to set(2,6), then (0,5) and (0,7) are open twins.
+    2. If a,b are stored in the open twins, delete a,b from the matrix in every other element in the same block.
+    eg, in this case, any element along row 0 except (0,5) and (0,7) can no longer store 2 or 6.
+    
+    Why does this work? Suppose A,B are two positions in the same block, and each of them can be a or b. Then we know it's either Aa and Bb or Ab and Ba. In either case, neither a nor b can fit into one of the other positions along the same block.
+    '''
     find_doubles = extracttuple(2)
     updates = set()
     for block in blocks:
         doubles = find_doubles(block, matrix)
-        twins = find_twins(doubles, matrix)
+        twins = []
+        if len(doubles) >= 2:
+            twins = find_twins(doubles, matrix)
         if len(twins) > 0:
             for pair in twins:
                 ups = tuple_ellimination(block, matrix, pair)
                 for u in ups:
                     updates.add(u)
     return updates
-    
+
+def open_triplets(blocks, matrix):
+    '''as with open_twins, but for positions with 3 possibilities instead of 2.'''
+    find_3tuples = extracttuple(3) #originally find_triples, but looked too much like "find_triplets"
+    updates = set()
+    for block in blocks:
+        triples = find_3tuples(block, matrix)
+        if len(triples)>=3:
+            triplets = find_triplets(triples, matrix)
+            if triplets:
+                ups = tuple_ellimination(block, matrix, triplets)
+                for u in ups:
+                    updates.add(u)
+    return updates
+            
 def output_board(matrix):
     board = [['.' for i in range(9)] for j in range(9)]
     for i in range(9):
@@ -191,7 +236,7 @@ def sudoku_solver(board):
     matrix, cancellation_queue = load_board(board)
     blocks = generate_all()
     while len(cancellation_queue) != 0:
-        matrix, cancellation_queue = canceller(matrix, cancellation_queue)
+        matrix = canceller(matrix, cancellation_queue)
         matrix, updates = full_counter(blocks, matrix)
         updates = updates.union(open_twins(blocks, matrix))
         cancellation_queue = list(updates)
